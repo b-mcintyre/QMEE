37ae4e42089cb50fc346e2378aeb69d889dba0d6
b-mcintyre <mcintb7@mcmaster.ca>
2021-01-31 16:14:51 -0500

    Exercise 2 README.md update and content.

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # QMEE
 Used for QMEE
 
+Exercise 2
+Main file to look at called: "means_SQ_TA" found in b-mcintyre/QMEE in the main branch
+data taken from dataset titled "NEW_CD_DGRO_Subset_Data_2019_V2"
+
+Main investigations include ensuring there is no duplicates in the total areas measurements as it was done by a macro
+implying that it is very unlikely a wing measure will be the same. If there are found duplicates I must got back to the
+raw image data and re-examine them to determine if they are duplicates.
+Seeing how many replicates were completed and ensuring that at least 2 replicates were completed among genotypes.
+Looking into the replicates to see how many there were and what they were. 
+Cleaning the data by created a genotype table, converting the pixel measurement into a mmsqr measurement, and creating
+a averages table for the semi-quantitative and total wing area in mmsqr based on genotype.
+I intend to use this table to conduct comparisons on variability inter-genetically on both a quantitative and 
+semi-quantitative level, as well as use the means to compare alleles of similar phenotypic effects. 
+To allow for reproducibility I broke my code into logical steps utilizing comments such as data checking and cleaning, 
+I placed comments above the code to state what I was doing, I titled my code in the script, I included the R version my
+code was created on in the script, I pipe-lined my code for the ability to easily break and edit my code, and I placed 
+the raw data utilized on github with this README file I stating where to find the code.  
+
   The data is a subset of data from a previous experiment conducted in Dr. Ian Dworkin's lab by MSc Caitlyn Daley utilizing D. melanogaster. The data contains crosses of 8 mutant alleles on 2 genes in Oregon-R genetic background with 20 wild type backgrounds from the Drosophila Genetic Research Panel (GDRP). The alleles range from very weak to very severe phenotypic effects on wing phenotype. Severity was measured quantitatively as total area in pixels and semi-quantiatively as morphoicial changes on a scale of 1-21 A score of 1 was given to wings appearing morphologically wild type and 21 given to wings with more severe morphological changes.
 
 The Biological questions I am attempting to answer are if background genetic effects are specific to individual alleles, correlated in alleles of the same gene, or correlated with genes of common phenotypic effects. I would also like to know the variance of background genetic effects between and among strains.
---
+++ means_SQ_TA.R
@@ -0,0 +1,140 @@
+#TITLE: Code for data checking, cleaning, coding an averages table based on genotypes, and plotting.  
+#code tested on R-4.0.3
+
+#download tidyverse
+library(tidyverse)
+
+#ensure tidyr and ggplot2 are in the library
+library(tidyr)
+library(ggplot2)
+
+# create a wing_table
+Wing_Table <- read_csv("NEW_CD_DGRP_Subset_Data_2019_V2.csv")
+
+###############################################################################
+# Beginning of data checking and cleaning
+
+
+summary(Wing_Table)
+options(dplyr.summarise.inform=FALSE) 
+
+#make list of what occurs with what and how many times
+print(Wing_Table %>%
+        group_by(Allele_1, WT_Background)%>%
+        summarise(count = n())
+      )
+
+print(Wing_Table %>%
+        group_by(Allele_1, WT_Background, Replicate) %>%
+      summarise(count = n())
+      )
+
+
+# Create genotype and add ID numbers to give relational info to raw data
+Wing_Table_Geno <- Wing_Table %>% unite(genotype, Allele_1,WT_Background) %>%
+  mutate(obs=seq(n()))
+
+#Add ID numbers to each row
+#Wing_Table_Geno$ID <-1:nrow(Wing_Table_Geno)
+  
+summary(Wing_Table_Geno)
+
+
+# Ensure at least 2 replicates per genotype
+print(Wing_Table_Geno %>%
+        group_by(Replicate, genotype)%>%
+        summarise(count= n()) %>%
+        filter(n()< 2)
+      )
+
+
+## Check if number of replicates is the same across each genotype
+print(Wing_Table_Geno %>%  
+        group_by(Replicate) %>%
+        summarise(count = n()))
+
+## what is R2a? Go back to raw data input and find out
+
+R2a <- Wing_Table_Geno %>%
+  group_by(Replicate,genotype) %>%
+  summarise(count = n())
+
+R2a <- dplyr::filter(R2a, Replicate == "R2a") #count of number R2a individuals per genotype 
+
+
+R2a_pos <- Wing_Table_Geno %>%
+  group_by(Replicate, genotype, obs) %>%
+  summarise()
+
+R2a_pos <- dplyr::filter(R2a_pos, Replicate == "R2a") # positional info on R2a individuals
+
+
+## what is R2a? Go back to raw data input and find out
+## R2a is a small replicate block added
+
+# see number of replicate individuals 
+print(Wing_Table_Geno %>%
+        group_by(genotype,Individuals) %>%
+      summarise(count = n ())) %>%
+      filter(count>2)
+# appears only one genotype is in replicate blocks 1,2, and 2a 
+
+
+# Find out if there is any duplicates of the wing area in px which is unlikely
+# due to the method of calculation so more likely an input error.
+# not applicable to SQ because they were measured semi-quantitatively so I would 
+# have many duplicates
+dup_dat <- Wing_Table_Geno %>% 
+        group_by(TotalArea.px,) %>%
+        filter(n()>1)
+
+dup_dat <- dplyr::select(dub_dat, genotype, TotalArea.px, obs)
+# double check wings to see if they are duplicate or if very unlikely 
+
+# write a csv in order to print and reference for manual check 
+write.csv(dup_dat, "./dup_dat")
+
+# convert total area in pixels to total area in mm^2
+px.mmsqr_conversion <- 0.00005375
+Wing_Table_Geno_mmsqr <- Wing_Table_Geno %>%
+  mutate(
+    TA_mmsqr = TotalArea.px * px.mmsqr_conversion,
+    TotalArea.px = NULL
+  )
+
+###############################################################################
+# now trying to get average table showing both the SQ and the total area
+
+
+Wing_Table_Geno_av <- Wing_Table_Geno_mmsqr %>%
+  group_by(genotype)%>%
+  dplyr::summarise(across(c(SQ_Measure, TA_mmsqr), mean)) %>%
+  dplyr::rename(SQ_avg = SQ_Measure, TA_avg_mmsqr = TA_mmsqr) %>%
+  print(Wing_Table_Geno_av)
+
+
+summary(Wing_Table_Geno_av)
+
+
+###############################################################################
+###############################################################################
+# plots+graphs code
+
+# see how total area follows SQ measures 
+TA_avgVsSQ_avgp <- plot (TA_avg_mmsqr~SQ_avg, data=Wing_Table_Geno_av)
+# good that it follows a downwards trend makes sense
+
+# see distribution of wing size over genotype looks like
+geno_wsb <- ggplot(data = Wing_Table_Geno_av,aes(x=genotype, y=TA_avg_mmsqr)) + 
+  geom_bar(stat = "identity")
+
+print(geno_wsb)
+
+# Same for SQ
+geno_sqb <- ggplot(data = Wing_Table_Geno_av, aes(x=genotype, y=SQ_avg)) +
+  geom_bar (stat = "identity")
+
+print(geno_sqb)
+# interesting that distribution is different between the two measurements 
+###############################################################################
+###############################################################################
